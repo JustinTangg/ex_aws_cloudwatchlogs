4ccd6dce05d54be6c50190281856d00c96409e21
Justin Tang <justin.tang@vonage.com>
2019-12-10 10:06:55 -0500

    additions to mix exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
 defmodule ExAws.CloudWatchLogs.MixProject do
   use Mix.Project
 
+  @version "1.0.0"
+
   def project do
     [
       app: :ex_aws_cloudwatchlogs,
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/JustinTangg/ex_aws_cloudwatchlogs",
+      homepage_url: "https://github.com/JustinTangg/ex_aws_cloudwatchlogs",
+      package: package(),
+      docs: [
+        main: "readme",
+        extras: ["README.md"],
+        source_ref: "v#{@version}"
+      ]
     ]
   end
 
@@ -26,6 +36,15 @@ defmodule ExAws.CloudWatchLogs.MixProject do
       {:poison, ">= 1.2.0", optional: true},
       {:ex_doc, "~> 0.19.2", only: [:dev, :test]},
       {:ex_aws, "~> 2.0"}
+    ]
+  end
+
+  defp package do
+    [
+      description: "AWS CloudWatchLogs service for ex_aws",
+      maintainers: ["Justin Tang"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/JustinTangg/ex_aws_cloudwatchlogs"}
     ]
   end
 end
